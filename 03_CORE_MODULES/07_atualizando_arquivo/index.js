5511de4343e3d8f3d313d580e8b73e87f4387caf
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-28 11:24:42 -0300

    Seção 03 - Core Modules - Atualizando arquivos

---
+++ 03_CORE_MODULES/07_atualizando_arquivo/index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+	const urlInfo = require('url').parse(req.url, true);
+	const nome = urlInfo.query.nome;
+
+	if(!nome){
+		fs.readFile('index.html', function(err, data){
+			res.writeHead(200,{'Content-Type': "text/html"});
+			res.write(data);
+			return res.end();
+		});
+	}else{
+
+		const nameNewLine = nome + "\r\n";
+
+		fs.appendFile("arquivo.txt", nameNewLine, function(err, data){
+			res.writeHead(302,{
+				location: "/"
+			})
+			res.end();
+		})
+	}	
+});
+
+server.listen(port, ()=>{
+	console.log(`servidor rodando na porta http://localhost:${port}`);
+})
