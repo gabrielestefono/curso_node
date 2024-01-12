87fb53123f2cfa4ddfd969cb3037eb922350a6f0
gabrielestefono <gabrielestefono@hotmail.com>
2024-01-12 07:00:52 -0300

    Seção 05 - Projeto Accounts - Aula 02

--- 05_ACCOUNTS/index.js
+++ 05_ACCOUNTS/index.js
@@ -5,4 +5,16 @@ const chalk = require('chalk');
 // Modules internos
 const fs = require('fs');
 
-console.log('Iniciamos o accounts')
+operation();
+
+function operation(){
+	inquirer.prompt([{
+		type: 'list',
+		name: 'action',
+		messagge: 'O que deseja fazer?',
+		choices: ['Criar conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair']
+	}]).then((answer)=>{
+		const action = answer['action'];
+		console.log(action);
+	}).catch((err)=>console.logg(err));
+}
