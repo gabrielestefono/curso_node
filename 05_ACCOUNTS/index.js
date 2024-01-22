fbded13f21ff4d13427363110e7328bb58472e4c
gabrielestefono <gabrielestefono@hotmail.com>
2024-01-22 08:45:05 -0300

    Seção 05 - Projeto Accounts - Aula 04

--- 05_ACCOUNTS/index.js
+++ 05_ACCOUNTS/index.js
@@ -26,4 +26,36 @@ function operation(){
 function createAnAccount(){
 	console.log(chalk.bgGreen.black("Parabéns por escolher o nosso banco!"));
 	console.log(chalk.green("Defina as opções da sua conta a seguir: "));
+	buildAccount();
+}
+
+function buildAccount(){
+	inquirer.prompt([
+		{
+			name: 'accountName',
+			message: 'Digite o nome para a sua conta: ',
+		}
+	]).then((answer) => {
+		const accountName = answer['accountName'];
+
+		console.info(accountName);
+
+		if(!fs.existsSync('accounts')){
+			fs.mkdirSync('accounts');
+		}
+
+		if(fs.existsSync(`accounts/${accountName}.json`)){
+			console.log(chalk.bgRed.black('Essa conta já existe, escolha outro nome!'));
+			buildAccount();
+			return;
+		}
+
+		fs.writeFileSync(`accounts/${accountName}.json`,
+			`{"balance": 0}`,
+			function(err){
+				console.log(err);
+			}
+		);
+		console.log(chalk.green(`Parabéns, a sua conta foi criada!`));
+	}).catch(err => console.log(err));
 }
