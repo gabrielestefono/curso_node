a66bed09e06cebb0740821ff2b83558550978938
gabrielestefono <gabrielestefono@hotmail.com>
2024-01-31 08:31:41 -0300

    Seção 07 - Introdução ao Express - Aula 04

---
+++ 07_TEMPLATE_ENGINE/04_condicionais/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.get('/dashboard', (req, res)=>{
+	res.render('dashboard');
+})
+
+app.get('/', (req, res) => {
+
+	const user = {
+		name: "Gabriel",
+		surname: "Estéfono",
+		age: 27
+	}
+
+	const palavra = "Teste";
+
+	const auth = false; 
+
+	res.render('home', {
+		user: user,
+		palavra: palavra,
+		auth: auth
+	});
+})
+
+
+app.listen(3000, ()=>{
+	console.log('http://localhost:3000')
+})
